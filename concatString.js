79cda2974937f01a6b5e6717e0f5ad2e11f774b9
loreliawand <loreliawand@gmail.com>
2022-06-27 19:05:08 +0200

    Added return

--- concatString.js
+++ concatString.js
@@ -1,12 +1,13 @@
 function concatString(str, sep) {
   let next = function (str, newSep, sep, acc) {
     if (typeof str != 'string') {
-      console.log(acc);
+      return acc;
     } else {
       if (typeof newSep == 'string') {
         sep = newSep;
       }
-      return (strNew, newSep) => next(strNew, newSep, sep, acc + str + sep);
+      acc = acc + str + sep;
+      return (str, newSep) => next(str, newSep, sep, acc);
     }
   };
   if (typeof sep != 'string') {
